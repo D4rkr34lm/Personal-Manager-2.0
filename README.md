dd5aaeda21136bd8a1e657f356fe0df277812ee5
D4rkr34lm <darth.m.frohn@gmail.com>
2024-03-04 00:31:41 +0100

    Created nav

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 ## Sources
 <a href="https://www.flaticon.com/de/kostenlose-icons/speisekarte" title="speisekarte Icons">Speisekarte Icons erstellt von SeyfDesigner - Flaticon</a> <br>
-<a href="https://www.flaticon.com/de/kostenlose-icons/anmeldung" title="anmeldung Icons">Anmeldung Icons erstellt von Freepik - Flaticon</a>
+<a href="https://www.flaticon.com/de/kostenlose-icons/anmeldung" title="anmeldung Icons">Anmeldung Icons erstellt von Freepik - Flaticon</a> <br>
+<a href="https://www.flaticon.com/de/kostenlose-icons/kalender" title="kalender Icons">Kalender Icons erstellt von rizky adhitya pradana - Flaticon</a> <br>
+<a href="https://www.flaticon.com/de/kostenlose-icons/email" title="email Icons">Email Icons erstellt von Freepik - Flaticon</a> <br>
+<a href="https://www.flaticon.com/de/kostenlose-icons/beschilderung" title="beschilderung Icons">Beschilderung Icons erstellt von Freepik - Flaticon</a>
 ## Project Setup
 
 ```sh
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
+
+const navActiv = ref(false)
 </script>
 
 <template>
   <header>
-    <button class="ui-button" id="nav-menu-button"><img class="header-icon" src="./assets/icons/speisekarte.png"></button>
+    <button class="ui-button" id="nav-menu-button" @click=" navActiv=!navActiv "><img class="header-icon" src="./assets/icons/speisekarte.png"></button>
     <div class="header-container">
       <img class="fh-icon" src="./assets/icons/fh.png">
       Software: Manganer
     </div>
     <button class="ui-button"><img class="header-icon" src="./assets/icons/einloggen.png"></button>
   </header>
-  <nav>
-    
+  <nav v-if="navActiv">
+    <RouterLink to="/calender" class="nav-link"><img class="nav-icon" src="./assets/icons/kalender.png"> Calender</RouterLink>
+    <RouterLink to="/email" class="nav-link"><img class="nav-icon" src="./assets/icons/mail.png"> Email</RouterLink>
+    <RouterLink to="/taskboard" class="nav-link"><img class="nav-icon" src="./assets/icons/beschilderung.png"> Taskboard</RouterLink>
   </nav>
+  <RouterView/>
   <footer>
 
   </footer>
 </template>
 
 <style scoped>
-  
-  
   header {
+    position: relative;
+    z-index: 10;
+
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -33,7 +40,6 @@ import { RouterLink, RouterView } from 'vue-router'
     background-color: var(--green-2);
 
     font-size: 7vh;
-    color: black;
 
     box-shadow: 0 0px 5px 0;
   }
@@ -67,10 +73,49 @@ import { RouterLink, RouterView } from 'vue-router'
   .ui-button:active {
     background-color: var(--green-3);
   }
-  
-  footer {
-    background-color: var(--green-5);
+
+  nav {
+    position: relative;
+    z-index: 0;
+
+    width: 5vw;
+    min-width: 200px;
+
+    box-shadow: 0 0px 5px 0 grey;
+    
+    
+    background-color: rgb(255, 255, 255);
+
+    min-height: 90vh;
   }
 
+  .nav-icon {
+    max-width: 5vh;
+    max-height: auto;
+
+    margin-right: 10px;
+  }
+
+  .nav-link{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    padding: 6px;
+    margin-bottom: 10px;
+
+    color: black;
+
+    font-size: 3vh;
+    text-decoration: none;
+  }
+
+  .nav-link:hover {
+    background-color: rgb(232, 232, 232);
+  }
+
+  .nav-link:active {
+    background-color: rgb(205, 205, 205);
+  }
   
 </style>
