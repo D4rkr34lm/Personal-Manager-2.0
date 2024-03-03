d294bf95be565421627c8e4143bc0a667b21a47d
D4rkr34lm <darth.m.frohn@gmail.com>
2024-03-03 11:12:08 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+  </header>
+  <nav>
+    
+  </nav>
+  <footer>
+
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
